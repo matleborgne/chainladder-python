467308ed8fe75741d3c71ab3738eb57984050626
matleborgne <mathieu.leborgne.pro@pm.me>
2025-04-18 15:58:06 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "casact/chainladder-python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        #"aarch64-linux"
+        #"x86_64-darwin"
+        #"aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f  {
+        pkgs = import nixpkgs { inherit system; };
+        python = with import nixpkgs { inherit system; }; python3.withPackages (ps: with ps; [ pygobject3 yt-dlp ]);
+      });
+
+    in
+    {
+      packages = forAllSystems ({ pkgs, python }: {
+
+        default = pkgs.stdenv.mkDerivation rec {        
+          name = "chainladder";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            desktop-file-utils
+            appstream-glib
+            meson
+            ninja
+            pkg-config
+            python3
+            wrapGAppsHook4
+          ];
+
+          buildInputs = with pkgs; [
+            haskellPackages.gi-gdk
+            haskellPackages.gi-gtk
+          ];
+
+          preFixup = ''
+            wrapProgram $out/bin/video-downloader \
+              --prefix PYTHONPATH : ${python}/${python.sitePackages} \
+          '';
+
+        };
+      });
+    };
+}
