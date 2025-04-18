5a19fef5e3c01c0a22aca93a614718eef4b9ffbf
matleborgne <mathieu.leborgne.pro@pm.me>
2025-04-18 16:51:18 +0200

    Update gitignore

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f  {
         pkgs = import nixpkgs { inherit system; };
-        #python = with import nixpkgs { inherit system; }; python3.withPackages (ps: with ps; [ chainladder ]);
+        python = with import nixpkgs { inherit system; }; python3.withPackages (ps: with ps; [ chainladder ]);
       });
 
     in
